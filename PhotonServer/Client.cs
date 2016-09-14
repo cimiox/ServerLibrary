58b804d22b0f93cbd87ece94d497452cc16193dd
cimiox <cimiox@outlook.com>
2016-09-14 20:01:47 +0300

    First commit in ServerLibrary

---
+++ PhotonServer/Client.cs
@@ -0,0 +1,51 @@
+﻿using Photon.SocketServer;
+using System;
+using PhotonHostRuntimeInterfaces;
+using ExitGames.Logging;
+using System.Collections.Generic;
+
+namespace PhotonServer
+{
+    class Client : ClientPeer
+    {
+        private readonly ILogger log = LogManager.GetCurrentClassLogger();
+
+        public Client(InitRequest initRequest) : base(initRequest)
+        {
+            log.Info("Player connection ip:" + initRequest.RemoteIP);
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            log.Debug("Disconnect");
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            switch (operationRequest.OperationCode)
+            {
+                case 1:
+                    if (operationRequest.Parameters.ContainsKey(1))
+                    {
+                        log.Debug("recive" + operationRequest.Parameters[1]);
+                        OperationResponse response = new OperationResponse(operationRequest.OperationCode);
+                        response.Parameters = new Dictionary<byte, object> { { 1, "response message" } };
+                        SendOperationResponse(response, sendParameters);
+                    }
+                    break;
+                case 2:
+                    if (operationRequest.Parameters.ContainsKey(1))
+                    {
+                        log.Debug("recive" + operationRequest.Parameters[1]);
+                        EventData eventData = new EventData(1);
+                        eventData.Parameters = new Dictionary<byte, object> { { 1, "response for event" } };
+                        SendEvent(eventData, sendParameters);
+                    }
+                    break;
+                default:
+                    log.Debug("Unknown OperationRequest received:" + operationRequest.OperationCode);
+                    break;
+            }
+        }
+    }
+}
---
+++ PhotonServer/Server.cs
@@ -0,0 +1,40 @@
+﻿using ExitGames.Logging;
+using ExitGames.Logging.Log4Net;
+using log4net.Config;
+using Photon.SocketServer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotonServer
+{
+    public class Server : ApplicationBase
+    {
+        private readonly ILogger log = LogManager.GetCurrentClassLogger();
+
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new Client(initRequest);
+        }
+
+        protected override void Setup()
+        {
+            var file = new FileInfo(Path.Combine(BinaryPath,"log4net.config"));
+            if (file.Exists)
+            {
+                LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);
+                XmlConfigurator.ConfigureAndWatch(file);
+            }
+
+            log.Debug("SERVER READY");
+        }
+
+        protected override void TearDown()
+        {
+            log.Debug("SERVER STOP");
+        }
+    }
+}
